f8f38f8cb26ea9d3dafc3124fee56cf1dd4b3dcd
Tony_S <schillertony@hotmail.de>
2023-02-25 19:09:09 +0100

    more responsive smaller than 992 but higher than 768, with burger-menu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
 
+  showMenu = false;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  showMediaMenu() {
+    this.showMenu = !this.showMenu;
+  }
 }

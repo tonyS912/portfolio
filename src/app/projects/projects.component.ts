26b04737ab075e2f862f9a3fe485f7ec2fc1b894
Tony_S <schillertony@hotmail.de>
2023-02-28 21:29:10 +0100

    put links into project

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-projects',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class ProjectsComponent {
 
+  constructor(private router: Router) { }
+
+  jumpToProject(url: string) {
+    window.open(url, '_blank');
+  }
 }
